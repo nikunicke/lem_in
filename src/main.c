6e02db073d598b5838b73db7a8a7d8d9e4a02db9
Niklas Pimenoff <pimenoff.niklas@gmail.com>
2020-10-06 13:45:25 +0300

    added main func with some notes

---
+++ src/main.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: npimenof <npimenof@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/06 13:32:45 by npimenof          #+#    #+#             */
+/*   Updated: 2020/10/06 13:43:05 by npimenof         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		main(void)
+{
+	// Read input
+	// Parse input into hash table and/or adjacency list
+	// Solve flow paths
+	// Pick paths
+	// Output result
+	ft_putendl("Hello World!");
+	
+	return (0);
+}
