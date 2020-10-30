a4c9a66eb48970bc9057348a3a2b866361a97cd1
Niklas Pimenoff <pimenoff.niklas@gmail.com>
2020-10-30 16:17:52 +0200

    removed some unnecessary stuff from costs.c. Added some files for .gitignore to track

--- src/network_flow/costs.c
+++ src/network_flow/costs.c
@@ -6,7 +6,7 @@
 /*   By: npimenof <npimenof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/22 17:15:58 by npimenof          #+#    #+#             */
-/*   Updated: 2020/10/30 13:27:40 by npimenof         ###   ########.fr       */
+/*   Updated: 2020/10/30 14:49:51 by npimenof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,33 +53,6 @@ int		min_cost_index(t_lem_in *data, t_list ***paths)
 		// printf("cost: %f\n", cost);
 	}
 	return (i_min - 1);
-}
-
-void	three_sixty_flip(t_list **set, int *ants, int flow, int moving_ants)
-{
-	int		i;
-	int		new_flow;
-	
-	i = 0;
-	new_flow = flow;
-	while (i < flow)
-	{
-		if (!--ants[i])
-			new_flow--;
-		i++;
-	}
-	if (!new_flow)
-		return ;
-	// printf("\t\t\t%d\n", new_flow);
-	// three_sixty_flip(set, ants, new_flow, moving_ants - new_flow);
-	// printf("wtf: %d\n", moving_ants - flow);
-	i = moving_ants - flow;
-	while (i < moving_ants)
-	{
-		printf("L%d-%s ", i + 1, ((t_edge *)set[i]->content)->from->id);
-		i++;
-	}
-	printf("\n");
 }
 
 int		update_flow(int	*distribution, int flow)
@@ -170,77 +143,18 @@ void	output_movement(t_list **set, int flow, int ants)
 		i--;
 	}
 	i = 0;
-	// while (i < flow)
-	// {
-	// 	printf("path%d len: %zu -- ants: %d\n", i, set[i]->content_size, ants_per_path[i]);
-	// 	// if (ants_per_path[i])
-	// 	// 	new_flow++;
-	// 	i++;
-	// }
-	// printf("ants: %d\n", ants);
-	// printf("new_flow: %d\n", new_flow);
-	// set[0] = set[0]->next;
-	// set[1] = set[1]->next;
-	// printf("set[0]: %s\n", ((t_edge *)set[0]->content)->from->id);
-	// printf("set[1]: %s\n", ((t_edge *)set[1]->content)->from->id);
-	// print_path(set[0]);
-	// print_path(set[1]);
 	t_list	**ant_loc;
 	int		j;
 	ant_loc = ft_malloctype(sizeof(t_list *), ants);
 	int moving_ants;
 
 	moving_ants = new_flow;
-	// while (i < new_flow)
-	// {
-	// 	ant_loc[i] = (set[i]->next->content);
-	// 	i++;
-	// }
-	// i = 0;
-	// while (ant_loc[i])
-	// {
-	// 	printf("i: %d -- '%s'\n", i, ant_loc[i]->from->id);
-	// 	i++;
-	// }
 	while (ant_loc[moving_ants - 1] != UNINITIALZED)
 	{
 		update_location(ant_loc, set, moving_ants, new_flow);
 		new_flow = update_flow(ants_per_path, new_flow);
 		moving_ants += new_flow;
 	}
-	
-	// update_location(ant_loc, set, new_flow, new_flow);
-	// printf("updated_flow: %d\n", (new_flow = update_flow(ants_per_path, new_flow)));
-	// update_location(ant_loc, set, new_flow + 2, new_flow);
-	// printf("updated_flow: %d\n", (new_flow = update_flow(ants_per_path, new_flow)));
-	// update_location(ant_loc, set, new_flow + 4, new_flow);
-	// printf("updated_flow: %d\n", (new_flow = update_flow(ants_per_path, new_flow)));
-	// update_location(ant_loc, set, new_flow + 6, new_flow);
-	// printf("updated_flow: %d\n", (new_flow = update_flow(ants_per_path, new_flow)));
-	// update_location(ant_loc, set, new_flow + 7, 1);
-	// printf("updated_flow: %d\n", (new_flow = update_flow(ants_per_path, new_flow)));
-	// printf("updated_flow: %d\n", (new_flow = update_flow(ants_per_path, new_flow)));
-
-	// three_sixty_flip(set, ants_per_path, new_flow, ants);
-
-	// char	***step;
-	// int		moving_ants;
-
-	// step = ft_malloctype(sizeof(char **), cost + 1);
-	// i = 0;
-	// while (i < (int)cost)
-	// {
-	// 	moving_ants = ((i + 1) * flow);
-	// 	step[i] = ft_malloctype(sizeof(char *), new_flow + 1);
-	// 	i++;
-	// }
-	// i = 0;
-	// while (i < new_flow)
-	// {
-	// 	printf("L%d-%s ", i + 1, ((t_edge *)set[i]->content)->from->id);
-	// 	i++;
-	// }
-
 }
 
 // cost could represent which path set we should use at which point? How do we account for previously "sent" ants?
